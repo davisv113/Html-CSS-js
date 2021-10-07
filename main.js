985b5be1bb3c2082d0050271afd4ba2042b12cd3
davisv113 <davi113@hotmail.com>
2021-10-07 01:12:38 -0300

    tentativa

---
+++ main.js
@@ -0,0 +1,13 @@
+//alert("Meu primeiro js")
+/*
+function clicou(){
+	alert("Obrigado por clicar")
+}*/
+
+/*function trocar(elemento){
+	elemento.innerHTML = "Você é muito lindo";
+}
+
+function voltar(elemento){
+	elemento.innerHTML = "Você é muito lindo";
+}*/
